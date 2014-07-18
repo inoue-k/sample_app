056a307fe16dea0b7816cf9a87b464ceba249f29
Uchio KONDO <udzura@pepabo.com>
2014-07-18 16:53:13 +0900

    Pry sample usage

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+gem 'pry'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "User pages" do
@@ -5,6 +6,9 @@ describe "User pages" do
   subject { page }
 
   describe "signup page" do
+    # テストの直前にブレークポイントをいれる
+    # before { binding.pry }
+
     before { visit signup_path }
 
     it { should have_content('Sign up') }
