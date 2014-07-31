db7afb02bd2c3e646cc576e2e49d37f9ccc04953
Uchio KONDO <udzura@pepabo.com>
2014-07-31 11:50:34 +0900

    Add bacobone sample page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ SampleApp::Application.routes.draw do
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+  get '/backbone-sample', to: 'static_pages#backbone'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
